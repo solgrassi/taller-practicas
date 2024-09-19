f3d5ee52497bc1b5ec89dd78653054799ef73748
Sol Grassi <grassisolgianna@gmail.com>
2024-09-19 17:16:39 -0300

    ejercicio 5.java

--- objetos/practica 1/ejercicio 5.java
+++ objetos/practica 1/ejercicio 5.java
@@ -1,13 +1,9 @@
-/*Escriba un programa que defina una matriz de enteros de tamaño 5x5. Inicialice
-la matriz con números aleatorios entre 0 y 30.
-Luego realice las siguientes operaciones:
-- Mostrar el contenido de la matriz en consola.
-- Calcular e informar la suma de los elementos de la fila 1
-- Generar un vector de 5 posiciones donde cada posición j contiene la suma
-de los elementos de la columna j de la matriz. Luego, imprima el vector.
-- Leer un valor entero e indicar si se encuentra o no en la matriz. En caso de
-encontrarse indique su ubicación (fila y columna) en caso contrario
-imprima “No se encontró el elemento”
+/*El dueño de un restaurante entrevista a cinco clientes y les pide que califiquen
+(con puntaje de 1 a 10) los siguientes aspectos: (0) Atención al cliente (1) Calidad
+de la comida (2) Precio (3) Ambiente.
+Escriba un programa que lea desde teclado las calificaciones de los cinco clientes
+para cada uno de los aspectos y almacene la información en una estructura. Luego
+imprima la calificación promedio obtenida por cada aspecto.
 */
 
 package tema1;
