682c39575dee28052a371a2afcf2a92601a08e24
Sol Grassi <grassisolgianna@gmail.com>
2024-09-22 18:06:42 -0300

    Ejercicio 2.java

---
+++ objetos/practica 2/Ejercicio 2.java
@@ -0,0 +1,55 @@
+package tema2;
+
+import PaqueteLectura.GeneradorAleatorio; 
+        
+public class TP2EJ2 {
+    
+    public static void main(String[] args) {
+        GeneradorAleatorio.iniciar();
+      
+        //variables
+        Persona [] vector = new Persona [14];
+       int DL =0;
+       int edad; int dni; String nombre;
+       int cont=0;
+       int i;
+       int min= 2000;
+       int aux=0;
+      
+       //cargar vector
+       edad= GeneradorAleatorio.generarInt(80);
+       while (edad != 0 && DL <14) {
+           vector[DL] = new Persona();
+           vector[DL].setEdad(edad);
+           nombre= GeneradorAleatorio.generarString(10);
+           vector[DL].setNombre(nombre);
+           dni = GeneradorAleatorio.generarInt(1000);
+           vector[DL].setDNI(dni); 
+           DL++;
+           edad= GeneradorAleatorio.generarInt(80);
+          
+       }
+       
+       //cant personas mayores a 65
+       for (i=0; i<DL; i++) {
+           if (vector[i].getEdad()> 65)
+               cont++;
+       }
+       System.out.println(" la cantidad de personas mayores a 65 años es de: " + cont);
+       
+       //persona con menor dni
+       for (i=0; i<DL; i++) {
+           if (vector[i].getDNI() < min) {
+               min= vector[i].getDNI();
+               aux=i;
+           }
+       }
+       System.out.println(vector[aux].toString());
+       
+       //control
+       for (i=0; i<DL; i++){
+           System.out.println(vector[i].toString());
+       }
+       
+    }
+}
