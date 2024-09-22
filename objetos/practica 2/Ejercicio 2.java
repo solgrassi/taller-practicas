0ed358e10dedd8605fe2e83c1969cd6e78686639
Sol Grassi <grassisolgianna@gmail.com>
2024-09-22 18:08:02 -0300

    Ejercicio 2.java

--- objetos/practica 2/Ejercicio 2.java
+++ objetos/practica 2/Ejercicio 2.java
@@ -1,3 +1,9 @@
+/*Utilizando la clase Persona. Realice un programa que almacene en un vector a lo sumo
+15 personas. La información (nombre, DNI, edad) se debe generar aleatoriamente hasta
+obtener edad 0. Luego de almacenar la información:
+- Informe la cantidad de personas mayores de 65 años.
+- Muestre la representación de la persona con menor DNI.*/
+
 package tema2;
 
 import PaqueteLectura.GeneradorAleatorio; 
